f72fd6408bd1032e846cfa918878ed843b4e84b6
alespages <69533332+alespages@users.noreply.github.com>
2020-09-19 11:18:03 +0200

    Programming Foundations
    Beginning with JavaScript

---
+++ ma1.js
@@ -0,0 +1,39 @@
+//1.Declare a variable.
+
+var firstVar;
+
+//2.Declare a variable called name.
+
+var name = "Maria";
+
+//3.Declare and initialise a variable with a number value.
+
+var mynumber = 10;
+
+//4.Variable called division with value of 20 divided by 5.
+
+var division = 20 / 5;
+
+//5.Code to checks type of the value "frog".
+
+var frogType = typeof "frog";
+
+//6.Declaration and inizialitation variable called orderHasShipped.
+
+var orderHasShipped = true;
+
+//7.If statement.
+
+if (orderHasShipped) {
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+//8.For loop from 0 to 9.
+
+var i;
+for(i=0; i<10; i++)
+{
+    console.log(i)
+}
